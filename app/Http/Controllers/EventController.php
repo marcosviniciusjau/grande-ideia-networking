110061ce18955cace70bdabf3c933f1a1cbf0085
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-07-25 13:51:09 -0300

    continuação das migrations

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,21 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
     public function index(){
 
-        $nome= "Matheus";
-        $idade=29;
-    
-        $arr= [1,2,3,4,5];
-        $nomes=["Matheus","Ally","Zoey","Melissa"];
-    
-        return view('welcome',
-        ['nome' => $nome,
-         'idade'=> $idade,
-         'arr'=> $arr,
-        'nomes'=> $nomes ]);
+       $events= Event::all();
+       
+        return view('welcome',['events'=>$events]);
     }
     public function create(){
         return view('events.create');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,31 +4,7 @@
 
 @section('content')
 
-       <h1>Algum titulo</h1>
-        <img src="/img/banner.jpg" alt="banner">
-        
-        <p>{{ $nome }}</p>
-        @if($nome == "Pedro")
-       <p>O nome é Pedro</p>
-       @elseif($nome =="Matheus")
-       <p>O nome é {{$nome}} e ele tem {{$idade}} anos</p>
-       @else
-        <p>O nome não é Pedro</p>
-        @endif
-
-        @for($i=0 ;$i < count($arr); $i++)
-        <p>{{$arr[$i]}} - {{$i}}</p>
-        
-        @endfor
-
-        @if($i ==2)
-            <p> O i é 2</p>
-      
-        @endif
-
-        @foreach($nomes as $nome)
-        <p>{{$loop->index}}</p>
-        <p>{{$nome}}</p>
-        @endforeach
-
+   @foreach($events as $event)
+   <p>{{$event->title}}- {{$event->description}}</p>
+   @endforeach
 @endsection
