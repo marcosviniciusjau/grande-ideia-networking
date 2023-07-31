b4bdd5609df8cbc1ea18b32ef71f1cee09eea2b9
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-07-31 14:11:19 -0300

    Adição da data do projeto vindo da tabela e css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,32 @@
 
 @section('content')
 
-   @foreach($events as $event)
-   <p>{{$event->title}}- {{$event->description}}</p>
-   @endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">{{ date('d/m/Y',strtotime($event->date)) }}</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="/events/{{$event->id}}" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
+
+        @if(count($events)==0)
+        <p>Não há eventos disponíveis</p>
+        @endif
+    </div>
+</div>
+
 @endsection
